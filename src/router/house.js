911779d54f3a1af5bb992b56bd9d97425bb06aad
mimmmji <taylor7418@gmail.com>
2024-09-17 18:24:57 +0900

    feat: 부동산 상세페이지 초안

--- src/router/house.js
+++ src/router/house.js
@@ -1,17 +1,22 @@
 export default [
   {
-    path: '/houses/maps/gosiwons',
-    name: 'houses/maps/gosiwons',
-    component: () => import('../views/House.vue'),
+    path: "/houses/maps/gosiwons",
+    name: "houses/maps/gosiwons",
+    component: () => import("../views/House.vue"),
   },
   {
-    path: '/houses/maps/onerooms',
-    name: 'houses/maps/onerooms',
-    component: () => import('../views/House.vue'),
+    path: "/houses/maps/onerooms",
+    name: "houses/maps/onerooms",
+    component: () => import("../views/House.vue"),
   },
   {
-    path: '/houses/maps/sharehouses',
-    name: 'houses/maps/sharehouses',
-    component: () => import('../views/House.vue'),
+    path: "/houses/maps/sharehouses",
+    name: "houses/maps/sharehouses",
+    component: () => import("../views/House.vue"),
+  },
+  {
+    path: "/houses/:id",
+    name: "DetailPage",
+    component: () => import("../views/DetailPage.vue"),
   },
 ];
