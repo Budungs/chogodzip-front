b68b5e20e08017e8facf28ace123d8d278e953a9
dongur2 <holymolyguri@gmail.com>
2024-09-19 17:22:15 +0900

    Feat: 청약 목록 탭 & 검색창 추가 + 테마 버튼 CSS 설정 수정

--- src/views/lotto/LottoList.vue
+++ src/views/lotto/LottoList.vue
@@ -1,10 +1,36 @@
 <template>
     <div class="container">
         <LottoTab />
-        <h1>청약목록</h1>
+        <div class="con-lotto-list">
+            <LottoListTab class="lotto-list-tab" />
+            <div class="con-lotto-search-list">
+                <LottoListSearch />
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
+import LottoListSearch from '@/modules/components/lotto/LottoListSearch.vue';
+import LottoListTab from '@/modules/components/lotto/LottoListTab.vue';
 import LottoTab from '@/modules/components/lotto/LottoTab.vue';
 </script>
+
+<style scoped>
+.con-lotto-list {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    gap:2%;
+    padding: 20px 0;
+}
+
+.lotto-list-tab {
+    flex: 1.5; 
+}
+
+.con-lotto-search-list {
+    flex: 9;
+    display: flex; /* 내부 요소가 필요하면 추가 */
+}
+</style>
