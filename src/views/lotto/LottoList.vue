a64411306e754f94026ee02f6979687515f48922
dongur2 <holymolyguri@gmail.com>
2024-09-19 17:30:50 +0900

    Feat: 청약 목록 총 페이지/한 페이지 글 개수 드롭다운 추가

--- src/views/lotto/LottoList.vue
+++ src/views/lotto/LottoList.vue
@@ -5,6 +5,18 @@
             <LottoListTab class="lotto-list-tab" />
             <div class="con-lotto-search-list">
                 <LottoListSearch />
+
+                <div style="width:100%; display:flex; flex-direction: row; justify-content: space-between;">
+                    <div class="txt-list-count-page" style="width:300px">
+                        전체 1020건 1/N페이지
+                    </div>
+                    <select class="form-select" aria-label="Default select example"  style="width:100px">
+                        <option selected>10건</option>
+                        <option value="1">10건</option>
+                        <option value="2">20건</option>
+                        <option value="3">30건</option>
+                    </select>
+                </div>
             </div>
         </div>
     </div>
@@ -31,6 +43,6 @@ import LottoTab from '@/modules/components/lotto/LottoTab.vue';
 
 .con-lotto-search-list {
     flex: 9;
-    display: flex; /* 내부 요소가 필요하면 추가 */
+    display: flex; flex-direction: column;
 }
 </style>
