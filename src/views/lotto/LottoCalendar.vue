a0da19b50903a2159b78b9f99195c99ee459f797
dongur2 <holymolyguri@gmail.com>
2024-09-19 17:21:22 +0900

    Feat: 청약 캘린더 전체/관심 청약 표시 버튼 추가, 테마 버튼 CSS 수정

--- src/views/lotto/LottoCalendar.vue
+++ src/views/lotto/LottoCalendar.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="container">
         <LottoTab />
+        <div class="btn-bx">
+            <button type="button" class="btn btn-primary"
+            style="width:150px; font-size:1.3rem; background-color: var(--main1); border: none">전체</button>
+            <button type="button" class="btn btn-secondary" style="width:150px; font-size:1.3rem">관심</button>
+        </div>
         <FullCalendar />
     </div>
 </template>
@@ -9,3 +14,12 @@
 import LottoTab from '@/modules/components/lotto/LottoTab.vue';
 import FullCalendar from '@/modules/components/lotto/FullCalendar.vue';
 </script>
+
+<style scoped>
+.btn-bx {
+    width:25%;
+    margin:2% 0 3% 0;
+    display:flex; 
+    justify-content: space-between; 
+}
+</style>
