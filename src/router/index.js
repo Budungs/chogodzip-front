8114c3152660ef5e14f7518cab013d74507dc5d0
HwangHyeonseok <hhs0995@naver.com>
2024-09-25 18:00:38 +0900

    feat:커뮤니티 swiper+초안

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import houseRoutes from './house';
 import lottoRoutes from './lotto';
-import registRoom from './registroom';
+import registRoom from './registRoom';
+import registMember from './registMember';
 import addinfo from '../views/login_regist/AddInfo.vue';
 import kakaoLogin from '../views/login_regist/KakaoLogin.vue';
 import registUser from '../views/login_regist/RegistUser.vue';
 import registComplete from '../views/login_regist/RegistComplete.vue';
+import CommunityMain from '@/views/community/CommunityMain.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,14 +18,13 @@ const router = createRouter({
   routes: [
     // 1안 ) 하나씩 라우팅 경로를 지정
     { path: '/', name: 'home', component: Home }, // 처음 들어올 때는 Home을 띄운다.
-    { path: '/addinfo', name:'addInfo', component: addinfo}, // AddInfo 페이지로 이동
-    { path: '/login', name:'kakaoLogin', component: kakaoLogin}, // AddInfo 페이지로 이동
-    { path: '/registuser', name:'registUser', component: registUser}, // RegistUser 페이지로 이동
-    { path: '/registcomplete', name:'registComplete', component: registComplete}, // RegistUser 페이지로 이동
+
+    { path: '/communitymain', name:'communityMain', component: CommunityMain}, // CommunityMain 페이지로 이동
     // 2안 ) 여러개의 경로를 묶을 수도 있다.
     ...houseRoutes,
       ...lottoRoutes,
       ...registRoom,
+      ...registMember,
     ],
   });
 
