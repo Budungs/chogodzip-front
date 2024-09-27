8bfc6357165cd46738d126f8bba68c17fa2fdc1f
HwangHyeonseok <hhs0995@naver.com>
2024-09-27 10:16:13 +0900

    feat:커뮤니티 메인 목록 + 커뮤니티 작성 폼 완성

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import registUser from '../views/login_regist/RegistUser.vue';
 import registComplete from '../views/login_regist/RegistComplete.vue';
 
 import CommunityMain from '@/views/community/CommunityMain.vue';
+import CommunityWrite from '@/views/community/CommunityWrite.vue';
 
 import helpRoutes from './help';
 
@@ -23,6 +24,7 @@ const router = createRouter({
     { path: '/', name: 'home', component: Home }, // 처음 들어올 때는 Home을 띄운다.
 
     { path: '/communitymain', name:'communityMain', component: CommunityMain}, // CommunityMain 페이지로 이동
+    { path: '/communitywrite', name:'communityWrite', component: CommunityWrite}, // CommunityWrite 페이지로 이동
     // 2안 ) 여러개의 경로를 묶을 수도 있다.
     ...houseRoutes,
       ...lottoRoutes,
