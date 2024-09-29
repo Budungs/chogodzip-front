8d8eb1297c33318e0c3208761c336bdf0aaac01a
dongur2 <holymolyguri@gmail.com>
2024-09-30 02:48:30 +0900

    Feat: Home02 관심 지역 통계 추가
    - 임시 통계 배경 이미지 추가

--- src/modules/components/home/LikeRegionState.vue
+++ src/modules/components/home/LikeRegionState.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="pb-4 pt-5 mb-2 mt-5" style="background-color:burlywood; height:60vh">
+    <div class="pb-4 pt-5 mb-4 mt-5" style="background-color:burlywood;">
         <!-- 타이틀 -->
         <div class="d-flex w-100 justify-content-center justify-content-lg-start mt-4 mb-4">
             <h2 class="h4 mb-0 d-flex w-100 justify-content-center"><span id="home-user-nickname">테스 </span> 의 관심 지역 현황을 확인해보세요 👾</h2>
@@ -7,17 +7,33 @@
 
         <!-- 본문 박스 -->
         <div class="row g-4 justify-content-around align-items-center">
+            <!-- 왼쪽 -->
             <div class="col-md-6">
                 <div class="border-0 h-100 w-100">
-                    <div class="d-flex w-100 h-50 flex-row justify-content-around align-items-center" style="background-color: pink;">
+                    <!-- 라우터 -->
+                    <div class="d-flex w-100 h-50 flex-row justify-content-around align-items-center">
                         <RouterButton href-url="/houses/maps/gosiwons" destination="고시원" backgroundImage="router-1" />
                         <RouterButton href-url="/houses/maps/room" destination="자취방" backgroundImage="router-2" />
                         <RouterButton href-url="/houses/maps/sharehouses" destination="공유주거공간" backgroundImage="router-3" />
                     </div>
 
-                    <div class="h-50" style="background-color:blue">down</div>
+                    <!-- 통계 -->
+                    <div class="h-50">
+                        <div class="card bg-size-cover bg-position-center border-0 overflow-hidden mb-4 mt-3 px-5 shadow rounded-3" style="background-image: url(src/assets/images/backgrounds/home-3.jpg);"><span class="img-gradient-overlay"></span>
+                            <div class="card-body content-overlay pb-0 mt-2">
+                                <a href="#"><span class="badge bg-dark fs-sm">지역 변경</span></a>
+                                <div class="h1 text-light mb-1 mt-2">서울시 광진구</div>
+                            </div>
+                            <div class="card-footer content-overlay border-0 pt-1 pb-4 d-flex flex-row justify-content-between">
+                                <StatsForLikeRegion buildingType="오피스텔" changeRateOfJeonse="0.921" changeRateOfRent="0.2" />
+                                <StatsForLikeRegion buildingType="연립/다세대" changeRateOfJeonse="-0.921" changeRateOfRent="0.129" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+            <!-- 오른쪽 -->
             <div class="col-md-6">
                 <div class="card bg-size-cover bg-position-center border-0 overflow-hidden h-100">map</div>
             </div>
@@ -27,6 +43,7 @@
 
 <script setup>
 import RouterButton from './RouterButton.vue';
+import StatsForLikeRegion from './StatsForLikeRegion.vue';
 </script>
 
 <style scoped>
---
+++ src/modules/components/home/StatsForLikeRegion.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="justify-content-around">
+        <h4 class="pt-0 pb-0" style="color:var(--main2)">{{ buildingType }}</h4>
+        <div>
+            <h6 class="d-flex mb-0 align-items-center" style="color:white"><span class="me-3" style="color:yellow">전세</span> 전월대비 
+                <span class="badge rounded-pill bg-dark px-2 mx-3" style="font-size: 0.625rem; min-width: 3vw; text-align: left">{{ changeRateOfJeonse >= 0 ? '🔺' : '🔻' }} {{ Math.abs(changeRateOfJeonse) }}%</span></h6>
+            <h6 class="d-flex mb-0 align-items-center" style="color:white"><span class="me-3" style="color:yellow">월세</span> 전월대비 
+                <span class="badge rounded-pill bg-dark px-2 mx-3" style="font-size: 0.625rem; min-width: 3vw; text-align: left">{{ changeRateOfRent >= 0 ? '🔺' : '🔻' }} {{ Math.abs(changeRateOfRent) }}%</span></h6>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    buildingType: {
+        type: String,
+        required: true,
+    },
+    changeRateOfJeonse: {
+        type: String,
+        required: true,
+    },
+    changeRateOfRent: {
+        type: String,
+        required: true,
+    },
+})
+
+const changeRateOfJeonse = parseFloat(props.changeRateOfJeonse);
+const changeRateOfRent = parseFloat(props.changeRateOfRent);
+</script>
