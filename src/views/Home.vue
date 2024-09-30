b68d3493061002dd4bb3c76f7dd26cab7e409ad8
dongur2 <holymolyguri@gmail.com>
2024-09-30 17:29:56 +0900

    Feat: Home03 관심 지역 최신 매물 목록 추가
    - 매물 카드 컴포넌트 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,12 @@
         <div class="container">
             <LikeRegionState />
         </div>
+        <LikeRegionLatest />
     </div>
 </template>
 
 <script setup>
+import LikeRegionLatest from '@/modules/components/home/LikeRegionLatest.vue';
 import LikeRegionState from '@/modules/components/home/LikeRegionState.vue';
 import SearchHouse from '@/modules/components/home/SearchHouse.vue';
 </script>
