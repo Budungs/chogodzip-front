34d0ffd4e90b13605b403125df4ee0ea94a2a104
dongur2 <holymolyguri@gmail.com>
2024-10-01 23:54:02 +0900

    Feat: Home04 청약 최신 정보 사진 & 공고 현황 카드 추가
    - 임시 이미지 추가: home-4

---
+++ src/modules/components/home/CounterCards.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="row justify-content-around">
+        <div class="col-md-4">
+            <div class="counter-box d-flex flex-column pt-5 pb-4 rounded-3 shadow">
+                <i class="fas fa-bullhorn display-6"></i>
+                <span class="counter display-5">120</span>
+                <p class="h4 mt-1">공고중</p>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="counter-box d-flex flex-column pt-5 pb-4 rounded-3 shadow">
+                <i class="fas fa-flag-checkered display-6"></i>
+                <span class="counter display-5" style="color:red">22</span>
+                <p class="h4 mt-1">접수중</p>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="counter-box d-flex flex-column pt-5 pb-4 rounded-3 shadow">
+                <i class="fas fa-pencil-alt display-6"></i>
+                <span class="counter display-5">10</span>
+                <p class="h4 mt-1">오늘 등록</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const counters = document.querySelectorAll('.counter');
+  counters.forEach(counter => {
+    let currentCounter = 0;
+    const target = parseInt(counter.innerText);
+    const duration = 4000;
+    const interval = 1000 / 60;
+    const step = target / (duration / interval);
+    
+    const updateCounter = () => {
+      currentCounter += step;
+      if (currentCounter < target) {
+        counter.innerText = Math.ceil(currentCounter);
+        requestAnimationFrame(updateCounter);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    
+    updateCounter();
+  });
+});
+</script>
+
+<style scoped>
+.counter-box {
+	background: #F7F8FA;
+	text-align: center
+}
+
+
+.counter-box p {
+	color: var(--gray1);
+}
+
+.counter {
+    color: var(--main1);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,16 @@
             <LikeRegionState />
         </div>
         <LikeRegionLatest />
+        <div class="container">
+            <LottosInfo />
+        </div>
     </div>
 </template>
 
 <script setup>
 import LikeRegionLatest from '@/modules/components/home/LikeRegionLatest.vue';
 import LikeRegionState from '@/modules/components/home/LikeRegionState.vue';
+import LottosInfo from '@/modules/components/home/LottosInfo.vue';
 import SearchHouse from '@/modules/components/home/SearchHouse.vue';
 </script>
 
