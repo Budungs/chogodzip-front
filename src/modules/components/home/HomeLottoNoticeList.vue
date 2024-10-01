7ca34ffadc2e0080490cb099ab6579c9355884e3
dongur2 <holymolyguri@gmail.com>
2024-10-02 00:32:18 +0900

    Feat: Home04 청약 최신 공고 목록 추가
    - 목록 & 아이템 컴포넌트 분리

---
+++ src/modules/components/home/HomeLottoNoticeList.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="card shadow-sm p-lg-3 mt-4 mb-lg-0">
+        <HomeLottoNoticeItem :noticeItems="noticeItems" />
+    </div>
+</template>
+
+<script setup>
+import HomeLottoNoticeItem from './HomeLottoNoticeItem.vue';
+
+const noticeItems = [
+    {
+        href: '#',
+        type: '행복주택(신혼희망)',
+        content: '아산탕정 2-A2BL 신혼희망타운 행복주택 자격완화 예비입주자 모집공고'
+    },
+    {
+        href: '#',
+        type: '공공분양(신혼희망)',
+        content: '인천계양 A3블록 신혼희망타운(공공분양) 입주자모집공고'
+    },
+    {
+        href: '#',
+        type: '임대상가(추첨)',
+        content: '부산금곡2단지 임대상가 입점자 모집공고'
+    },
+    {
+        href: '#',
+        type: '매입임대',
+        content: '경기 군포시 기숙사형 청년주택 예비입주자 모집공고'
+    },
+    {
+        href: '#',
+        type: '행복주택(신혼희망)',
+        content: '아산탕정 2-A2BL 신혼희망타운 행복주택 자격완화 예비입주자 모집공고'
+    },
+    {
+        href: '#',
+        type: '공공분양(신혼희망)',
+        content: '인천계양 A3블록 신혼희망타운(공공분양) 입주자모집공고'
+    },
+    {
+        href: '#',
+        type: '임대상가(추첨)',
+        content: '부산금곡2단지 임대상가 입점자 모집공고'
+    },
+    {
+        href: '#',
+        type: '매입임대',
+        content: '경기 군포시 기숙사형 청년주택 예비입주자 모집공고'
+    },
+]
+</script>
--- src/modules/components/home/LottosInfo.vue
+++ src/modules/components/home/LottosInfo.vue
@@ -3,23 +3,23 @@
         <div class="home-04-title w-100 mt-2 =mb-4">
             <h1 class="h2">오늘의 청약 최신 정보를 확인하세요 📢</h1>
         </div>
-        <div class="row g-3 justify-content-between align-items-center" style="background-color: red;">
+        <div class="row g-3 justify-content-between align-items-center">
             <!-- 왼쪽 사진 -->
-            <div class="col-md-4 h-100 overflow-hidden" style="background-color: antiquewhite;">
-                <img class="rounded-3 shadow" src="@/assets/images/backgrounds/home-4.jpg" style="height:60vh; max-height:650px; width: 20vw; min-width: 300px" alt="Cover">
+            <div class="col-md-4 h-100 overflow-hidden">
+                <img class="rounded-3 shadow" src="@/assets/images/backgrounds/home-4.jpg" style="height:80vh; max-height:650px; width: 20vw; min-width: 300px" alt="Cover">
             </div>
 
             <!-- 오른쪽 -->
-            <div class="col-md-8" style="background-color: yellow;">
+            <div class="col-md-8">
                 <div class="h-100 w-100">
                     <!-- 공고 현황 -->
-                     <div class="h-50 w-100"  style="background-color: yellowgreen;">
+                     <div class="h-50 w-100">
                         <CounterCards />
                     </div>
                     
                     <!-- 최신 청약 공고 목록 -->
-                    <div class="h-75 w-100" style="background-color: aquamarine;">
-                        
+                    <div class="h-75 w-100">
+                        <HomeLottoNoticeList />
                     </div>
                 </div>
             </div>
@@ -29,4 +29,5 @@
 
 <script setup>
 import CounterCards from './CounterCards.vue';
+import HomeLottoNoticeList from './HomeLottoNoticeList.vue';
 </script>
