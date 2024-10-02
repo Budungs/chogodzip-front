fb118ea464da388254983726b480f6a91c141934
dongur2 <holymolyguri@gmail.com>
2024-10-02 17:40:00 +0900

    Refactor: 매물 등록 폼 수정중 - 전체 UI 구조 구성 완료

---
+++ src/views/Postroom copy.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="container border border rounded shadow mt-5 mb-5 pt-5 pb-5">
+
+    <h1 class="h1">매물 정보</h1>
+
+    <!-- 매물정보 - 임대 유형 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center;">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >임대 유형</label>
+      </div>
+      <div class="col-lg-3 col-sm-5">
+        <select class="form-select">
+          <option>임대 유형을 선택하세요.</option>
+          <option>월세</option>
+          <option>전세</option>
+        </select>
+      </div>
+    </div>
+
+    <!-- 매물정보 - 보증금 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >보증금</label>
+      </div>
+      <input class="form-control" type="number" id="text-input" placeholder="보증금을 입력해주세요.">만원
+    </div>
+
+    <!-- 매물정보 - 관리비 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >관리비</label>
+      </div>
+      <input class="form-control" type="number" id="text-input" placeholder="관리비를 입력해주세요.">만원
+    </div>
+
+    <!-- 매물정보 - 해당층/전체층 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >해당층/전체층</label>
+      </div>
+      <input class="form-control" type="number" id="text-input" placeholder="해당층을 입력해주세요.">층
+      <input class="form-control" type="number" id="text-input" placeholder="전체층을 입력해주세요." style="margin-left:20px;">층
+    </div>
+
+    <!-- 매물정보 - 방 구조 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center;">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >방 구조</label>
+      </div>
+      <div class="col-lg-3 col-sm-5">
+        <select class="form-select">
+          <option>방 구조를 선택하세요.</option>
+          <option>원룸(오픈형)</option>
+          <option>원룸(분리형)</option>
+          <option>복층 원룸</option>
+          <option>오피스텔 (원룸형)</option>
+        </select>
+      </div>
+    </div>
+
+    <!-- 매물정보 - 방 수/욕실 수 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >방 수/욕실 수</label>
+      </div>
+      <input class="form-control" type="number" id="text-input" placeholder="방 개수를 입력해주세요.">개
+      <input class="form-control" type="number" id="text-input" placeholder="욕실 개수를 입력해주세요." style="margin-left:20px;">개
+    </div>
+
+    <!-- 매물정보 - 면적 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >면적</label>
+      </div>
+      <input class="form-control" type="number" id="text-input" placeholder="전용 평수를 입력해주세요.">평
+      <input class="form-control" type="number" id="text-input" placeholder="공급 평수를 입력해주세요." style="margin-left:20px;">평
+    </div>
+
+    <!-- 매물정보 - 주실 방향 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center;">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >주실 방향</label>
+      </div>
+      <div class="col-lg-3 col-sm-5">
+        <select class="form-select">
+          <option>주실 방향을 입력하세요.(안방 기준)</option>
+          <option>동향</option>
+          <option>서향</option>
+          <option>남향</option>
+          <option>북향</option>
+        </select>
+      </div>
+    </div>
+    
+    <!-- 매물정보 - 최소계약기간 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >최소계약기간</label>
+      </div>
+      <input class="form-control" type="number" id="text-input" placeholder="최소계약기간을 입력해주세요.">개월
+    </div>
+    
+    <!-- 매물정보 - 입주가능일 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >입주가능일</label>
+      </div>
+      <input class="form-control" type="text" id="text-input" placeholder="입주가능일을 입력해주세요.">
+    </div>
+
+    <!-- 매물정보 - 주차 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >주차</label>
+      </div>
+      <button type="button" class="btn btn-sm btn-danger" 
+      :style="{backgroundColor: isParkingAvailable ? 'blue': 'gray', color: 'white', borderColor: 'transparent'}"
+      @click="toggleParkingStatus">
+      {{ isParkingAvailable ? '주차 가능' : '주차 불가능' }}
+      </button>
+
+      <input class="form-control" type="text" id="text-input" placeholder="월 주차 금액을 입력해주세요." style="margin-left:15px;"
+      :disabled="!isParkingAvailable" v-model="parkingFee">만원
+    </div>
+
+    <h1 class="h1">대출</h1>
+
+    <!-- 매물정보 - 대출 종류 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center;">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >대출 종류</label>
+      </div>
+      <div class="col-lg-3 col-sm-5">
+        <select class="form-select">
+          <option>대출 종류를 선택하세요.</option>
+          <option>HUG 주택도시보증공사 대출</option>
+          <option>중소기업취업청년 대출(중기청) 80%</option>
+          <option>중소기업취업청년 대출(중기청) 100%</option>
+          <option>버팀목 대출</option>
+        </select>
+      </div>
+    </div>
+
+    <h1 class="h1">시설 정보</h1>
+
+    <!-- 매물정보 - 난방시설 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center;">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >난방시설</label>
+      </div>
+      <div class="col-lg-3 col-sm-5">
+        <select class="form-select">
+          <option>난방시설 방식을 선택하세요.</option>
+          <option>중앙 난방</option>
+          <option>개별 난방</option>
+          <option>가스 난방</option>
+        </select>
+      </div>
+    </div>
+
+    <!-- 매물정보 - 냉방시설 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center;">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >냉방시설</label>
+      </div>
+      <div class="col-lg-3 col-sm-5">
+        <select class="form-select">
+          <option>냉방시설 방식을 선택하세요.</option>
+          <option>벽걸이 에어컨</option>
+          <option>스탠드형 에어컨</option>
+          <option>천장형 에어컨</option>
+        </select>
+      </div>
+    </div>
+
+    <!-- 매물정보 - 생활시설 -->
+    <div class="w-100 mb-3 d-flex h-100" style="align-items:center;">
+      <div class="h-100 d-flex justify-content-left align-items-center" style="width:13%">
+        <label for="text-input" class="d-flex h-100" >생활시설</label>
+      </div>
+      <div class="d-flex">
+        <button
+          v-for="(item, index) in items"
+          :key="index"
+          type="button"
+          class="btn btn-sm"
+          :class="item.selected ? 'btn-primary' : 'btn-secondary'"
+          @click="toggleSelection(item)"
+        >
+          {{ item.name }}
+        </button>
+      </div>
+
+    </div>
+
+
+
+  </div>
+
+
+</template>
+
+<style scoped>
+h1 {
+  margin-top:30px;
+  margin-bottom: 20px;
+}
+.form-control {
+  width:310px;
+  margin-right:10px;
+}
+
+label {
+  font-size: 1.25rem;
+}
+
+button {
+  margin-right : 10px;
+}
+</style>
+
+<script setup>
+import { ref } from 'vue';
+
+// 주차 가능 버튼 로직
+const isParkingAvailable = ref(true); // 주차 가능 여부 상태
+const parkingFee = ref(''); // 주차 요금
+
+// 상태 반전을 위한 함수
+const toggleParkingStatus = () => {
+  isParkingAvailable.value = !isParkingAvailable.value;
+
+  if (!isParkingAvailable.value) { // 주차 불가능 상태가 되면 input 필드의 내용을 지운다.
+    parkingFee.value = '';
+  }
+};
+
+
+// 시설 정보 - 생활시설 선택 로직
+
+
+
+</script>
--- src/views/Postroom.vue
+++ src/views/Postroom.vue
@@ -1,288 +1,151 @@
-<!-- 방 내놓기 페이지 -->
 <template>
-    <div class="container">
-      <div class="property-info">
-        <h1>방내놓기</h1>
-        <!-- 매물 정보 -->
-        <h2>매물 정보</h2>
-        <hr />
-        <table>
-          <tr>
-            <th rowspan="2">매물 기초정보<span class="star">*</span></th>
-            <td>
-              매물명
-              <input class="large-input" type="text" placeholder="리스트에 노출되는 매물명은 40자 이내로 작성해 주세요." />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              매물 유형
-              <div class="property-type">
-                <button :class="{ active: selectedType === '고시원' }" @click="selectType('고시원')">고시원</button>
-                <button :class="{ active: selectedType === '쉐어하우스' }" @click="selectType('쉐어하우스')">쉐어하우스</button>
-                <button :class="{ active: selectedType === '공유주택' }" @click="selectType('공유주택')">공유주택</button>
-                <button :class="{ active: selectedType === '원룸' }" @click="selectType('원룸')">원룸</button>
-                <button :class="{ active: selectedType === '빌라/투룸' }" @click="selectType('빌라/투룸')">빌라/투룸</button>
-                <button :class="{ active: selectedType === '오피스텔' }" @click="selectType('오피스텔')">오피스텔</button>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <th rowspan="3">매물 주소<span class="star">*</span></th>
-          </tr>
-          <tr>
-            <td>
-              주소 검색
-              <div class="address-search">
-                <input class="large-input" type="text" placeholder="예) 반동 10-1, 강북구 번동" />
-                <button>검색</button>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div class="input-group">
-                <label>동 입력</label>
-                <div class="input-with-margin">
-                  <input class="small-input" type="text" placeholder="예) 101" /> 동
-                  <input class="small-input" type="text" placeholder="예) 101" /> 호
-                </div>
-              </div>
-              <div>
-                <input type="checkbox" id="check" />
-                <label for="check">등기부등본 상에 ‘동’ 정보가 없을 경우 체크</label>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <th>주변 정보</th>
-            <td>
-              <div class="subway-info">
-                <input class="large-input" type="text" placeholder="주변 지하철역을 입력해 주세요." />
-                <button>검색</button>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <th>방 정보</th>
-            <td>
-              <div class="input-group">
-                <label>방 수</label>
-                <div class="number-input">
-                  <input class="small-input" type="number" min="0" placeholder=" " />
-                  <span class="unit">개</span>
-                </div>
-              </div>
-            </td>
-          </tr>
-        </table>
-    
-        <br><br>
-        <!-- 가격 정보 -->
-        <h2>가격 정보</h2>
-        <hr>
-        
-        <table>
-          <tr>
-            <th width="250px">거래 종류<span class="star">*</span></th>
-            <td>
-              <div class="radio-group">
-                <label>
-                  <input type="radio" name="transactionType" value="전세" checked /> 전세
-                </label>
-                <label>
-                  <input type="radio" name="transactionType" value="월세" /> 월세
-                </label>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <th rowspan="2">가격 정보<span class="star">*</span></th>
-          </tr>
-          <tr>
-            <td>
-              전세 <br>
-              <input class="small-input" type="number" placeholder=" " /> 만원
-              <span> ~ </span>
-              <input class="small-input" type="number" placeholder=" " /> 만원
-              <br><br>
-              보증금 <br>
-              <input class="small-input" type="number" placeholder=" " /> 만원
-              <span> ~ </span>
-              <input class="small-input" type="number" placeholder=" " /> 만원
-            </td>
-          </tr>
-        </table>
-    
-        <!-- 사진 등록 -->
-        <br><br>
-        <h2>사진 등록</h2>
-        <hr>
-        <table>
-          <tr>
-            <th width="250px">일반 사진<span class="star">*</span></th>
-            <td>          
-              <button class="add-photo-button" @click="triggerFileInput">+ 사진 추가</button>
-              <input type="file" accept="image/*" multiple ref="fileInput" style="display: none;" @change="handleFileUpload" />
-            </td>
-          </tr>
-        </table>
-    
-        <!-- 태그 -->
-        <br><br>
-        <h2>태그</h2>
-        <hr>
-        <table>
-          <th width="250px">태그<span class="star">*</span></th>
-          <td><input class="large-input" type="text" placeholder="태그를 입력해주세요. #으로 구분하여 입력해주세요."></td>
-        </table>
-    
-        <!-- 매물 등록 버튼 -->
-        <br><br>
-        <div class="register-button-container">
-          <!-- <button class="register-button">매물 등록</button> -->
-          <button type="button" class="btn btn-light">Info</button>
+  <div class="container mt-0 mb-md-4 py-5" >
+    <div class="row">
+
+      <div class="col-lg-8">
+        <!-- Breadcrumb-->
+        <nav class="mb-3 pt-md-3" aria-label="Breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">방 내놓기</li>
+            <li class="breadcrumb-item active" aria-current="page">매물 정보 등록</li>
+          </ol>
+        </nav>
+
+
+        <!-- Title-->
+        <div class="mb-4">
+          <h1 class="h2 mb-3">매물 정보 등록</h1>
+          <p class="mb-0"><span class="text-danger">*</span>표시는 필수 입력 사항입니다.</p>
+          <div class="d-lg-none pt-3 mb-2">65% content filled</div>
+          <div class="progress d-lg-none mb-4" style="height: .25rem;">
+            <div class="progress-bar bg-warning" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
         </div>
+
+
+        <!-- Basic info-->
+        <section class="card card-body border-0 shadow-sm p-4 mb-4" id="room-category">
+          <h2 class="h4 mb-4"><i class="fas fa-tags text-primary fs-5 mt-n1 me-2" />매물 유형을 선택해주세요.</h2>
+          <div class="row">
+            <div class="col-sm-6 mb-3">
+              <label class="form-label" for="ap-category">매물 유형 <span class="text-danger">*</span></label>
+              <select class="form-select" id="ap-category" required>
+                <option value="" disabled selected>매물 유형을 선택해주세요.</option>
+                <option value="gosiwon">고시원</option>
+                <option value="jachiroom">자취방</option>
+                <option value="sharehouse">공유주거공간</option>
+              </select>
+            </div>
+          </div>
+        </section>
+
+
+
+        <!-- 임대 유형 -->
+        <section class="card card-body border-0 shadow-sm p-4 mb-4" id="room-category">
+          <h2 class="h4 mb-4"><i class="fas fa-tags text-primary fs-5 mt-n1 me-2" />이용 정보를 입력해주세요.</h2>
+          <div class="row">
+
+            <div class="col-sm-6 mb-3 w-100">
+              <div class="form-label pt-3 pb-2">임대 유형<span class="text-danger">*</span></div>
+              <div class="container row w-100">
+                <div class="form-check col-lg-4 justify-content-around">
+                  <input class="form-check-input" type="radio" id="ap-company" name="ap-business-type">
+                  <label class="form-check-label" for="ap-company">1일</label>
+                </div>
+                <div class="form-check col-lg-4">
+                  <input class="form-check-input" type="radio" id="ap-individual" name="ap-business-type">
+                  <label class="form-check-label" for="ap-individual">1주일</label>
+                </div>
+                <div class="form-check col-lg-4">
+                  <input class="form-check-input" type="radio" id="ap-individual" name="ap-business-type">
+                  <label class="form-check-label" for="ap-individual">1개월(월세)</label>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="col-sm-6 mb-3 w-100">
+              <h3 class="form-label pt-3 pb-2">임대 가격</h3>
+              <div class="form-label pt-3 pb-2">보증금<span class="text-danger">*</span></div>
+              <div class="d-flex align-items-center">
+                <div class="w-50 pe-2">
+                  <div class="input-group"><span class="input-group-text fs-base">$</span>
+                    <input class="form-control range-slider-value-min" type="text">
+                  </div>
+                </div>
+                <div class="text-muted">&mdash;</div>
+                <div class="w-50 ps-2">
+                  <div class="input-group"><span class="input-group-text fs-base">$</span>
+                    <input class="form-control range-slider-value-max" type="text">
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-label pt-3 pb-2">관리비<span class="text-danger">*</span></div>
+              <div class="d-flex align-items-center">
+                <div class="w-50 pe-2">
+                  <div class="input-group"><span class="input-group-text fs-base">$</span>
+                    <input class="form-control range-slider-value-min" type="text">
+                  </div>
+                </div>
+                <div class="text-muted">&mdash;</div>
+                <div class="w-50 ps-2">
+                  <div class="input-group"><span class="input-group-text fs-base">$</span>
+                    <input class="form-control range-slider-value-max" type="text">
+                  </div>
+                </div>
+              </div>
+           </div>
+
+
+
+
+          </div>
+
+          <!-- 월세 -->
+          <label class="form-label" for="ap-price">임대 가격<span class="text-danger">*</span></label>
+          <div class="d-sm-flex">
+            <select class="form-select w-25 mb-2">
+              <option value="day">1일</option>
+              <option value="week">1주</option>
+              <option value="month">1개월</option>
+            </select>
+            
+            <select class="form-select w-25 mb-2">
+              <option value="month">월세</option>
+              <option value="year">전세</option>  
+            </select>
+            <input class="form-control w-100 me-2 mb-2" type="number" id="ap-price" min="200" step="50" required>
+
+          </div>
+
+        </section>
+
+
       </div>
+
+      <!-- Progress of completion-->
+      <aside class="col-lg-3 offset-lg-1 d-none d-lg-block">
+        <div class="sticky-top pt-5">
+          <h6 class="pt-5 mt-3 mb-2">65% content filled</h6>
+          <div class="progress mb-4" style="height: .25rem;">
+            <div class="progress-bar bg-warning" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <ul class="list-unstyled">
+            <li class="d-flex align-items-center">
+              <i class="far fa-check-circle text-primary me-2"/>
+              <a class="nav-link fw-normal ps-1 p-0" href="#room-category" data-scroll data-scroll-offset="20">매물 유형</a>
+            </li>
+            <li class="d-flex align-items-center"><i class="far fa-check-circle text-primary me-2"></i><a class="nav-link fw-normal ps-1 p-0" href="#location" data-scroll data-scroll-offset="20">Location</a></li>
+            <li class="d-flex align-items-center"><i class="far fa-check-circle text-primary me-2"></i><a class="nav-link fw-normal ps-1 p-0" href="#details" data-scroll data-scroll-offset="20">Property details</a></li>
+            <li class="d-flex align-items-center"><i class="far fa-check-circle text-muted me-2"></i><a class="nav-link fw-normal ps-1 p-0" href="#price" data-scroll data-scroll-offset="20">Price</a></li>
+            <li class="d-flex align-items-center"><i class="far fa-check-circle text-muted me-2"></i><a class="nav-link fw-normal ps-1 p-0" href="#photos" data-scroll data-scroll-offset="20">Photos / video</a></li>
+            <li class="d-flex align-items-center"><i class="far fa-check-circle text-primary me-2"></i><a class="nav-link fw-normal ps-1 p-0" href="#contacts" data-scroll data-scroll-offset="20">Contacts</a></li>
+          </ul>
+        </div>
+      </aside>
+
     </div>
-  
-  </template>
-  
-    
-    <script>
-    export default {
-      data() {
-        return {
-          selectedType: '쉐어하우스' // 기본 선택된 매물 유형
-        };
-      },
-      methods: {
-        selectType(type) {
-          this.selectedType = type;
-        },
-        triggerFileInput() {
-        this.$refs.fileInput.click(); // 파일 입력 클릭 이벤트 트리거
-        },
-        handleFileUpload(event) {
-          const files = event.target.files;
-          // 파일 업로드 처리 로직을 여기에 추가
-          console.log(files); // 선택한 파일을 콘솔에 출력 (예시)
-        }
-      }
-    }
-    </script>
-    
-    <style scoped>
-    .property-info {
-      font-family: Arial, sans-serif;
-      margin: 20px;
-    }
-    
-    h2 {
-      margin: 10px 0; /* 제목과 줄 간격 조정 */
-    }
-    
-    table {
-      width: 100%;
-      border-collapse: collapse;
-    }
-    
-    th, td {
-      border: 1px solid #ccc;
-      padding: 10px;
-      text-align: left;
-    }
-    
-    h1 {
-      text-align: center;
-    }
-    
-    .property-type {
-      display: flex;
-      flex-wrap: wrap;
-      gap: 10px;
-    }
-    
-    .address-search {
-      display: flex;
-      gap: 10px;
-      margin-top: 10px;
-    }
-    
-    .input-group {
-      display: flex;
-      flex-direction: column;
-      margin-top: 10px;
-    }
-    
-    .input-with-margin {
-      margin-top: 10px; /* 한 줄 간격 추가 */
-      display: flex;
-      gap: 10px;
-    }
-    
-    .subway-info {
-      display: flex;
-      gap: 10px;
-      margin-top: 10px;
-    }
-    
-    .number-input {
-      display: flex;
-      align-items: center;
-    }
-    
-    .large-input {
-      width: 400px;
-    }
-    .small-input {
-      width: 100px;
-    }
-    
-    span.unit {
-      margin-left: 7px; /* 개와 입력란 사이 간격 */
-    }
-    .star { /* 필수 입력을 위한 * 표시  */
-      color:#a28cd1;
-    }
-  
-    .radio-group { 
-      display: flex;
-      gap: 20px; /* 라디오 버튼 간격 조정 */
-      margin-top: 10px; /* 상단 여백 추가 */
-    }
-  
-    /* 매물 등록 버튼 */
-    /* .register-button {
-      background-color:#a28cd1;
-      color: white;
-      width: 200px;
-      height: 70px;
-    } */
-    .register-button:hover {
-      background-color:#dfcef7;
-    }
-    .register-button-container {
-      display: flex; /* Flexbox 활성화 */
-      justify-content: center; /* 수평 가운데 정렬 */
-    }
-  
-    button {
-      padding: 10px 15px;
-      border: none;
-      border-radius: 5px;
-      background-color: #f0f0f0;
-      cursor: pointer;
-      transition: background-color 0.3s;
-    }
-    
-    button:hover {
-      background-color: #e0e0e0;
-    }
-    
-    button.active {
-      background-color: #a28cd1; /* 강조된 색상 */
-      color: white;
-    }
-  
-    </style>
+  </div>
+</template>
