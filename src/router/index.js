9cecaeb6742c0bbcd46c879ec6bfe124d8cb1e06
mimmmji <taylor7418@gmail.com>
2024-10-10 15:36:57 +0900

    Fix: 깃 충돌

--- src/router/index.js
+++ src/router/index.js
@@ -12,11 +12,7 @@ import roomRoutes from './registRoom';
 import userRoutes from './user';
 import authRoutes from './auth';
 import Chat from '@/views/Chat.vue';
-<<<<<<< HEAD
 import ChatTest from '@/views/ChatTest.vue';
-=======
-import errorRoutes from './error';
->>>>>>> 37ff5374aefb4238bf9e5897d6b5e78886a76e11
 
 
 const router = createRouter({
