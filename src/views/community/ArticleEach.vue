a88f3da5e1c500657fea3c7617ad9a8d5c771436
dongur2 <holymolyguri@gmail.com>
2024-10-11 15:26:30 +0900

    Feat: 커뮤니티 태그 매핑 추가

--- src/views/community/ArticleEach.vue
+++ src/views/community/ArticleEach.vue
@@ -1,7 +1,7 @@
 <template>
     <tr>
         <td style="width:5%; text-align:center;" >{{ item.communityId }}</td>
-        <td style="width:10%; text-align:center;">{{ item.tag === 'RE' ? '부동산' : item.tag }}</td>
+        <td style="width:10%; text-align:center;">{{ getTagName(item.tag) }}</td>
         <td style="width:50%; text-align:center;">
             <router-link :to="{ path: `/community/${item.communityId}` }" style="text-decoration: none;">{{ item.title }}</router-link>
         </td>
@@ -22,6 +22,22 @@ const props = defineProps({
         requried: true,
     }
 })
+
+//태그
+const tagMapping = {
+    'REPI': '부동산 정책/투자',
+    'REHT': '부동산 핫이슈',
+    'RERV': '부동산 후기',
+    'CTRV': '계약/입주 후기',
+    'ITRV': '인테리어 후기',
+    'LNQS': '대출 질문',
+    'LTQS': '분양/청약 질문'
+};
+
+const getTagName = (tag) => {
+    return tagMapping[tag] || tag;
+};
+
 </script>
 
 <style scoped>
